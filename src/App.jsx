018f715d767ecbf1ea9372c575cbd28b29de9fed
Milena Nobre <mnmbrito@hotmail.com>
2022-05-06 15:50:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import React from 'react'
+
+
+export default () => (
+     <div className="App">
+        <h1>Desafio 1 - Calculadora Simples</h1>
+         <div className="Container">
+
+         </div>
+    </div>
+)
